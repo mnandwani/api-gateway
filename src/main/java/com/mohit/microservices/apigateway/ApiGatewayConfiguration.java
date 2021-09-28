d178ffc579dd8e0fe053ce386aa7d90dcd890419
VSPL\mnandwani <mohit.nandwani@vicsuper.com.au>
2021-09-28 11:40:10 +1000

    first commit

---
+++ src/main/java/com/mohit/microservices/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,39 @@
+package com.mohit.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://CURRENCY-EXCHANGE-SERVICE"))
+                .route(p -> p.path("/currency-converter/**")
+                        .uri("lb://CURRENCY-CONVERSION-SERVICE"))
+                .route(p -> p.path("/currency-converter-feign/**")
+                        .uri("lb://CURRENCY-CONVERSION-SERVICE"))
+                .route(p -> p.path("/currency-converter-new/**")
+                        .filters(f -> f.rewritePath(
+                                "/currency-converter-new/(?<segment>.*)",
+                                "/currency-converter-feign/${segment}"))
+                        .uri("lb://CURRENCY-CONVERSION-SERVICE"))
+                .build();
+
+    }
+}
